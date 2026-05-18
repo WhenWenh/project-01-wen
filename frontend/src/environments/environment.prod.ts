deb7f40feae008743d4f1c63960491ab787d7496
YusufKhj <yusof.khouja@gmail.com>
2026-05-18 20:50:08 +0200

    feat: frontend-only pipeline with deploy to Azure App Service

---
+++ frontend/src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  apiBaseUrl: 'https://tourplanner-backend-b7drdyc8bcc9fzgu.italynorth-01.azurewebsites.net/api',
+  useMockAuth: false
+} as const;
