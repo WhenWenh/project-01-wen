d9cb3965fc2bc62ed62c51b49192e1abc2f1e116
maxim <if25b201@technikum-wien.at>
2026-05-19 17:55:28 +0200

    Yaml Config

--- frontend/src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  apiBaseUrl: 'https://tourplanner-backend-b7drdyc8bcc9fzgu.italynorth-01.azurewebsites.net/api',
-  useMockAuth: false
-} as const;
